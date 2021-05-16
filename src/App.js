088f0d03b93cb7d20098ca31343b6ecc57b3cc89
biggersmatt <biggersmatt73@gmail.com>
2021-05-16 16:26:50 -0700

    Adds create new party page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 import Login from "./pages/Login/Login";
+import CreateParty from "./pages/CreateParty/CreateParty";
 
 import "./App.css";
 
@@ -13,6 +14,9 @@ function App() {
         <Switch>
           <Route exact path="/">
             <Login></Login>
+          </Route>
+          <Route path="/createparty">
+            <CreateParty></CreateParty>
           </Route>
         </Switch>
       </div>
---
+++ src/pages/CreateParty/CreateParty.js
@@ -0,0 +1,11 @@
+require("./CreateParty.css");
+
+function CreateParty() {
+  return (
+    <div>
+      <h1>Create a New Party</h1>
+    </div>
+  )
+}
+
+export default CreateParty;
