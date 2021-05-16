b539abb16f16199a1c585e067e3c318e1cece104
biggersmatt <biggersmatt73@gmail.com>
2021-05-16 16:21:41 -0700

    Creates and links login page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import Login from "./pages/Login/Login";
+
+import "./App.css";
 
 function App() {
   return (
     <div>
       <h1>Cards With Corgis</h1>
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <Login></Login>
+          </Route>
+        </Switch>
+      </div>
     </div>
   );
 }
---
+++ src/pages/Login/Login.js
@@ -0,0 +1,11 @@
+require("./Login.css");
+
+function Login() {
+  return (
+    <div>
+      <h1>Login Page</h1>
+    </div>
+  )
+}
+
+export default Login;
