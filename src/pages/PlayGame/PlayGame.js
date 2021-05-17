0982335923f92658cfbe1c49a0bf4b2a25aaba97
biggersmatt <biggersmatt73@gmail.com>
2021-05-16 21:04:27 -0700

    Adds html and css to PlayGame page

--- src/pages/PlayGame/PlayGame.js
+++ src/pages/PlayGame/PlayGame.js
@@ -2,8 +2,13 @@ require("./PlayGame.css")
 
 function PlayGame() {
   return (
-    <div className="playgame-content">
-      <h1>Play Game Page</h1>
+    <div className="playgame-container">
+      <h1>"Party Name"</h1>
+      <div className="playgame-wrapper">
+        <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, qui?</h4>
+        <h5 className="playgame-author">Created By: "firstName"</h5>
+      </div>
+      <button className="playgame-next-btn">Next</button>
     </div>
   )
 }
