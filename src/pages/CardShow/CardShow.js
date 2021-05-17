72f9e8387245e29fb04c30b287cef3047a46ae55
biggersmatt <biggersmatt73@gmail.com>
2021-05-17 15:49:49 -0700

    Adds html and css to CardShow page

--- src/pages/CardShow/CardShow.js
+++ src/pages/CardShow/CardShow.js
@@ -2,8 +2,23 @@ require("./CardShow.css");
 
 function CardShow() {
   return (
-    <div className="card-show-content">
-      <h1>Card Show Page</h1>
+    <div className="card-show-container">
+      <header>
+        <h3>Edit Your Cards</h3>
+        <p>You can edit/update this card.</p>
+      </header>
+      <div className="card-show-wrapper">
+        <h3>Update This Card</h3>
+        <textarea 
+          name="prompt" 
+          id="prompt" 
+          cols="30" 
+          rows="10"
+          placeholder="Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, qui?"
+          >
+          </textarea>
+        <button>Update Card</button>
+      </div>
     </div>
   )
 }
