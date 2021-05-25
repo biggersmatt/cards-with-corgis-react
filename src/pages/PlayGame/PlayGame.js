8c9f3cfeda4744282888fb3ecfd1b967769f5f55
biggersmatt <biggersmatt73@gmail.com>
2021-05-25 16:29:54 -0700

    Removes opacity from corgi photo

--- src/pages/PlayGame/PlayGame.js
+++ src/pages/PlayGame/PlayGame.js
@@ -92,7 +92,7 @@ function PlayGame(props) {
       <div className="playgame-wrapper" style={divStyle}>
         <div className="playgame-prompt-bg">
           <h4>{cards.activeCard ? cards.activeCard.prompt : "No cards have been created for this deck yet."}</h4>
-          <h5 className="playgame-author">{cards.activeCard ? `Created by: ${cards.activeCard.author}` : null}</h5>
+          <h5>{cards.activeCard ? `Created by: ${cards.activeCard.author}` : null}</h5>
         </div>
       </div>
       {cards.activeCard ? 
