c5e245e446532219c6b92e1050eb5122c7d345e7
biggersmatt <biggersmatt73@gmail.com>
2021-05-26 15:44:58 -0700

    Adds state to ExistingCard component to Redirect

--- src/components/ExistingCard/ExistingCard.js
+++ src/components/ExistingCard/ExistingCard.js
@@ -1,8 +1,12 @@
 // React
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
+
+require("./ExistingCard.css");
 
 function ExistingCard(props) {
+
+  let [redirect, setRedirect] = useState(null);
 
   const handleDelete = () => {
     fetch(`http://localhost:4000/card/${props.cardId}`, {
@@ -15,12 +19,30 @@ function ExistingCard(props) {
     .then(() => props.handleExistingCards())
     .catch((err) => console.log(err))
   }
+
+  if(redirect) {
+    return <Redirect to={redirect} />
+  }
   
   return (
-    <div className="manage-this-card" key={props.cardId}>
-      <p>{props.prompt}</p>
-      <Link to={`/playorcreate/create/${props.cardId}`}>Edit</Link>
-      <button onClick={handleDelete}>Delete</button>
+    <div className="manage-this-card shadow" key={props.cardId}>
+      <div className="manage-card-prompt">
+        <p>{props.prompt}</p>
+      </div>
+      <div className="manage-card-btns">
+        <button 
+          onClick={() => setRedirect(redirect = `/playorcreate/create/${props.cardId}`)}
+          className="manage-card-btn"
+        >
+        Edit
+        </button>
+        <button 
+          onClick={handleDelete}
+          className="manage-card-btn"
+        >
+        Delete
+        </button>
+      </div>
     </div>
   )
 }
