35d7e2893d30b824dbab3295e34c9d7bcb5f048e
biggersmatt <biggersmatt73@gmail.com>
2021-05-26 17:30:15 -0700

    Modifies bgc on pages

--- src/pages/PlayOrCreate/PlayOrCreate.js
+++ src/pages/PlayOrCreate/PlayOrCreate.js
@@ -14,28 +14,28 @@ function PlayOrCreate(props) {
 
   return (
     <div className="play-or-create-container">
-      <header className="play-or-create-header">
-        <h3>Deck: {props.partyName}!</h3>
-        <p>"Play" shuffles all the cards in this deck so you can play Cards With Corgis!</p>
-        <p>"Cards" allows you to create new cards for this deck or edit/remove cards that you have already created.</p>
-        <p>Cards you have created will be searched for by the first name that you provided. It is case sensitive!</p>
-      </header>
-
-      <div className="play-or-create-wrapper">
-        <button 
-          className="play-or-create-btn"
-          onClick={() => setRedirect(redirect = "/playorcreate/play")}
-        >
-          Play
-        </button>
-        <button 
-          className="play-or-create-btn"
-          onClick={() => setRedirect(redirect = "/playorcreate/create")}
-        >
-          Cards
-        </button>
+      <div className="play-or-create-shader">
+        <header className="play-or-create-header">
+          <h3>Deck: {props.partyName}!</h3>
+          <p>"Play" shuffles all the cards in this deck so you can play Cards With Corgis!</p>
+          <p>"Cards" allows you to create new cards for this deck or edit/remove cards that you have already created.</p>
+          <p>Cards you have created will be searched for by the first name that you provided. It is case sensitive!</p>
+        </header>
+        <div className="play-or-create-wrapper">
+          <button 
+            className="play-or-create-btn"
+            onClick={() => setRedirect(redirect = "/playorcreate/play")}
+          >
+            Play
+          </button>
+          <button 
+            className="play-or-create-btn"
+            onClick={() => setRedirect(redirect = "/playorcreate/create")}
+          >
+            Cards
+          </button>
+        </div>
       </div>
-
     </div>
   )
 }
