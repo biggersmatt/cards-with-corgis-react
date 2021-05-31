8dd458a4e648d0cdcaa30a09358bb1de8ebfe229
biggersmatt <biggersmatt73@gmail.com>
2021-05-30 20:45:37 -0700

    Adds compare similarity between prompts on CreateCard page

--- src/pages/CreateCard/CreateCard.js
+++ src/pages/CreateCard/CreateCard.js
@@ -7,6 +7,7 @@ import ExistingCard from "../../components/ExistingCard/ExistingCard";
 // CSS
 require("./CreateCard.css");
 
+
 function CreateCard(props) {
   let [cardPage, setCardPage] = useState({
     prompt: "",
@@ -17,6 +18,8 @@ function CreateCard(props) {
     handleExistingCards();
   // eslint-disable-next-line react-hooks/exhaustive-deps
   },[])
+
+  let stringSimilarity = require("string-similarity");
 
   const handleExistingCards = () => {
     let filteredCards = [];
@@ -38,29 +41,54 @@ function CreateCard(props) {
     .catch(err => console.log(err));
   }
 
+  const handleSimilarityCheck = (filteredCards) => {
+    for(let i = 0; i < filteredCards.length; i++) {
+      let similarity = stringSimilarity.compareTwoStrings(filteredCards[i].prompt, cardPage.prompt);
+      if(similarity >= 0.8) {
+        console.log("Over 80% similarity between your card and another card in the deck")
+        return true;
+      }
+    }
+    return false;
+  }
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    if(cardPage.prompt) {
-      const newCard = {
-        prompt: cardPage.prompt,
-        author: props.firstName,
-        discard: false,
-        partyId: props.userId,
-      }
-      fetch("https://pacific-mesa-89997.herokuapp.com/card", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(newCard),
+    fetch("https://pacific-mesa-89997.herokuapp.com/card")
+    .then(response => response.json())
+    .then(jsonData => {
+      const allCards = jsonData.allCards;
+      let filteredCards = [];
+      allCards.forEach(card => {
+        if(card.partyId === props.userId) {
+          filteredCards.push(card);
+        }
       })
-      .then(() => alert("New Card Created"))
-      .then(() => props.addedNewCard())
-      .then(() => handleExistingCards())
-      .catch(err => console.log(err))
-    } else {
-      alert("Cannot Create a Card Without Text.")
-    }
+      const similar = handleSimilarityCheck(filteredCards);
+      if(cardPage.prompt && !similar) {
+        const newCard = {
+          prompt: cardPage.prompt,
+          author: props.firstName,
+          discard: false,
+          partyId: props.userId,
+        }
+        fetch("https://pacific-mesa-89997.herokuapp.com/card", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newCard),
+        })
+        .then(() => alert("New Card Created"))
+        .then(() => props.addedNewCard())
+        .then(() => handleExistingCards())
+        .catch(err => console.log(err))
+      } else if(similar) {
+        alert("This card is too similar to one already in the deck. Be more unique!")
+      } else {
+        alert("Cannot Create a Card Without Text.")
+      }
+    })
   }
 
   const handleChange = (event) => {
