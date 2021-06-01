a48a76bec908d3f12471d7a316b34c68d9529d5f
biggersmatt <biggersmatt73@gmail.com>
2021-06-01 11:36:52 -0700

    Adds ternaries to Playgame page

--- src/pages/PlayGame/PlayGame.js
+++ src/pages/PlayGame/PlayGame.js
@@ -95,16 +95,18 @@ function PlayGame(props) {
     backgroundSize: "cover",
   };
 
+  const active = false;
+
   return (
     <div className="playgame-container shadow">
       <div className="playgame-wrapper" style={divStyle}>
         <h1 className="playgame-deck-name">{props.partyName} Deck</h1>
-        <div className="playgame-wildcard wildcard-active">
+        <div className={`playgame-wildcard ${active ? "wildcard-active" : null}`}>
           <div className="playgame-prompt-bg">
-            <h4 className="playgame-prompt hidden">{cards.activeCard ? cards.activeCard.prompt : "No cards have been created for this deck yet."}</h4>
-            <h6 className="playgame-author hidden">{cards.activeCard ? `Created by: ${cards.activeCard.author}` : null}</h6>
-            <h4 className="wildcard-rule-header">Make a Permenant Rule!</h4>
-            <h6 className="wildcard-rule-footer">A Majority of Players Have to Agree That The Rule is Fun and Fair</h6>
+            <h4 className={`playgame-prompt ${active ? "hidden" : null}`}>{cards.activeCard ? cards.activeCard.prompt : "No cards have been created for this deck yet."}</h4>
+            <h6 className={`playgame-author ${active ? "hidden" : null}`}>{cards.activeCard ? `Created by: ${cards.activeCard.author}` : null}</h6>
+            <h4 className={`wildcard-rule-header ${!active ? "hidden" : null}`}>Make a Permenant Rule!</h4>
+            <h6 className={`wildcard-rule-footer ${!active ? "hidden" : null}`}>A Majority of Players Have to Agree That The Rule is Fun and Fair</h6>
           </div>
         </div>
       </div>
